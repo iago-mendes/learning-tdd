d18a8c265c3de0f5b7788890df8ae764f02d0625
Iago <iagobrazmendes@gmail.com>
2021-06-11 17:20:41 -0300

    handle private route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const routes = require('express').Router()
 
 const session = require('./app/controllers/session')
+const authMiddleware = require('./app/middleware/auth')
 
 routes.post('/sessions', session.store)
+
+routes.use(authMiddleware)
+
+routes.get('/dashboard', (req, res) => res.status(200).send())
 
 module.exports = routes
