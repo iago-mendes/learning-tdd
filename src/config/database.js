68ed6d14588a1c9136e58db2b1734cc2e498c39e
Iago <iagobrazmendes@gmail.com>
2021-06-11 15:25:44 -0300

    connect to postgres

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports =
+{
+	host: '127.0.0.1',
+	username: 'nodeauth',
+	password: 123,
+	database: 'nodeauth',
+	dialect: 'postgres',
+	logging: false,
+	define:
+	{
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true
+	}
+}
