12c41212b90339a4c45f2e62f8fc695c655d6d55
Iago <iagobrazmendes@gmail.com>
2021-06-11 14:43:25 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+import routes from './routes'
+
+class AppController
+{
+	constructor()
+	{
+		this.express = express()
+
+		this.middlewares()
+		this.routes()
+	}
+
+	middlewares()
+	{
+		this.express.use(express.json())
+	}
+
+	routes()
+	{
+		this.express.use(routes)
+	}
+}
+
+export default new AppController().express
